03259af2efcd66e545ab911e1cb8d350e563882d
hisa <s1521038@cco.kanagawa-it.ac.jp>
2018-06-15 17:12:36 +0900

    send_coin.cssを変更

--- hisa/js/send_coin.js
+++ hisa/js/send_coin.js
@@ -4,15 +4,19 @@ var noBtn = document.querySelector('#no');
 var coinbox = document.querySelector('#coinbox');
 
 yesBtn.onclick = function(){
-    if(window.confirm(coinbox.value + " pen-D 送金します。\nよろしいですか？")){
-        console.log("送金しました。");
-        
-    }else{
-        alert("キャンセルしました。");
+    if(coinbox.value == ""){
+        alert('送金額を入力してください！');
     }
-    
+    else{
+        if(window.confirm(coinbox.value + " pen-D 送金します。\nよろしいですか？")){
+            console.log(coinbox.value + " pen-D 送金しました。");
+        
+        }else{
+            alert("キャンセルしました。");
+        }
+    }
 }
 
 noBtn.onclick = function(){
-    location.href = './home.html';
+    location.href = './index.html';
 }
