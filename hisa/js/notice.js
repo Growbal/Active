11f9cb44cbaffca4f546e16a58609f0588b785a8
hisa <s1521038@cco.kanagawa-it.ac.jp>
2018-07-06 20:52:36 +0900

    通知画面の途中経過

--- hisa/js/notice.js
+++ hisa/js/notice.js
@@ -1,5 +1,35 @@
 let noticeBtn = document.querySelector('#notice');
 
 noticeBtn.onclick = function(){
-  document.body.appendChild('<div id="modal"></div>');
+  fadeIn(document.querySelector('.modal'),300);
+}
+
+function fadeIn(node, duration) {
+  // display: noneでないときは何もしない
+  if (getComputedStyle(node).display !== 'none') return;
+
+  // style属性にdisplay: noneが設定されていたとき
+  if (node.style.display === 'none') {
+    node.style.display = '';
+  } else {
+    node.style.display = 'block';
+  }
+  node.style.opacity = 0;
+
+  var start = performance.now();
+
+  requestAnimationFrame(function tick(timestamp) {
+    // イージング計算式（linear）
+    var easing = (timestamp - start) / duration;
+
+    // opacityが1を超えないように
+    node.style.opacity = Math.min(easing, 1);
+
+    // opacityが1より小さいとき
+    if (easing < 1) {
+      requestAnimationFrame(tick);
+    } else {
+      node.style.opacity = '';
+    }
+  });
 }
